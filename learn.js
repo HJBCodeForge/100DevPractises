f26c5f3dfe222d3097c595715d7da619d9d10abb
Henning <hjb.codeforge@gmail.com>
2025-08-26 11:12:11 -0400

    practise on async and await

--- learn.js
+++ learn.js
@@ -1,20 +1,52 @@
-function checkWeather (callback) {
-    const weather = () => Math.random() < 0.5 ? "sunny" : "rainy"
+// function checkWeather (callback) {
+//     const weather = () => Math.random() < 0.5 ? "sunny" : "rainy"
 
-    let error = weather()
-    console.log(error)
+//     let error = weather()
+//     console.log(error)
 
-    if(error === "sunny") {
-        callback(null, "Go for a walk")
-    } else {
-        callback("Stay inside")
-    }
+//     if(error === "sunny") {
+//         callback(null, "Go for a walk")
+//     } else {
+//         callback("Stay inside")
+//     }
+// }
+
+// checkWeather(function(err, message) {
+//     if(err) {
+//         console.log("Error: " + err)
+//     } else {
+//         console.log(message)
+//     }
+// })
+
+// function flipCoin() { 
+//     return new Promise(function(resolve, reject){
+//         let randomNum = Math.random()
+//         if(randomNum < 0.5){
+//             resolve("Heads")
+//         } else {
+//             reject("Tails")
+//         }
+// })
+// }
+
+// flipCoin()
+//     .then(result => console.log("Succes: " + result))
+//     .catch(error => console.log("Error: " + error))
+
+function getTemperature(city) {
+    return new Promise((resolve) => {
+        setTimeout(() => {resolve("25°C in " + city)}, 1000)
+    })
 }
 
-checkWeather(function(err, message) {
-    if(err) {
-        console.log("Error: " + err)
-    } else {
-        console.log(message)
+async function showTemperature(city) {
+    try {
+        let cityTemp = await getTemperature(city)
+        console.log(cityTemp)  
+    } catch(error) {
+        console.log("Error: " + error)
     }
-})
+    
+}
+showTemperature("Montreal")
